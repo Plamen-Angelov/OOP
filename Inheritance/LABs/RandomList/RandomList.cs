e84a11865ef555cf2746c7e05e6a61de9134c8ec
Plamen Angelov <plamen565@gmail.com>
2021-06-29 10:55:27 +0300

    Inheritance - Lab
    Add all problems from LAB

---
+++ Inheritance/LABs/RandomList/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private readonly Random random;
+        public string RandomString()
+        {
+            int index = random.Next(0, this.Count);
+            string result = this[index];
+            this.RemoveAt(index);
+
+            return result;
+        }
+    }
+}

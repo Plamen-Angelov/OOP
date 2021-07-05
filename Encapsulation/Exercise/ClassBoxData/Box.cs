4d90f15f09e3035a63f5623a4ca3ed5f6bb856ac
Plamen Angelov <plamen565@gmail.com>
2021-07-05 23:12:04 +0300

    Encapsulation Exercise
    Add some problems

---
+++ Encapsulation/Exercise/ClassBoxData/Box.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double lenght;
+        private double width;
+        private double height;
+
+
+        public double Lenght
+        {
+            get { return lenght; }
+            private set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Lenght cannot be zero or negative.");
+                }
+                this.lenght = value; 
+            }
+        }
+
+
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                this.width = value;
+            }
+        }
+
+
+        public double Height
+        {
+            get { return height; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                this.height = value;
+            }
+        }
+
+
+        public Box(double lenght, double width, double height)
+        {
+            Lenght = lenght;
+            Width = width;
+            Height = height;
+        }
+
+        public double GetSurfaceArea()
+        {
+            return (2 * Lenght * Width) + (2 * Lenght * Height) + (2 * Width * Height);
+
+
+        } 
+
+        public double GetLateralSurfaceArea()
+        {
+            return (2 * Lenght * Height) + (2 * Width * Height);
+        }
+
+        public double GetVolume()
+        {
+            return Lenght * Width * Height;
+        }
+    }
+}
---
+++ Encapsulation/Exercise/ClassBoxData/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(lenght, width, height);
+                
+                Console.WriteLine($"Surface Area - {box.GetSurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.GetLateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.GetVolume():F2}");
+            }
+            catch (ArgumentException ae)
+            {
+
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}

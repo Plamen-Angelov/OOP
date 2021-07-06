234d0473ac2731024f16c5aa9a05d41ddcd1af9a
Plamen Angelov <plamen565@gmail.com>
2021-07-06 14:29:02 +0300

    Interfaces and Abstractions
    Add problems from LAB

---
+++ Interfaces and Abstraction/LAB/Shapes/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine(new string('*', Width));
+            for (int i = 1; i <= Height - 2; i++)
+            {
+                Console.WriteLine($"*{new string(' ', Width-2)}*");
+            }
+            Console.WriteLine(new string('*', Width));
+        }
+    }
+}

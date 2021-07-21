57bab64de5b400f3190c6e44c2d9ff09aa5a698c
Plamen Angelov <plamen565@gmail.com>
2021-07-21 14:35:15 +0300

    Add to Reflection and Attributes
    Add problems

---
+++ Reflection And Attributes/LAB/CodeTracker/AuthorAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    public class AuthorAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public AuthorAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}

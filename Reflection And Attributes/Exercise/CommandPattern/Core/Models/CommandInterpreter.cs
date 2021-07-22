4efdf25f0341e00918e03f6376bc18777a79a522
Plamen Angelov <plamen565@gmail.com>
2021-07-22 12:12:05 +0300

    Add problem
    Add problem to Reflection and attributes - exercise

---
+++ Reflection And Attributes/Exercise/CommandPattern/Core/Models/CommandInterpreter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+
+namespace CommandPattern.Core.Models
+{
+    using Contracts;
+    using System.Linq;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] tokens = args
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string commandName = tokens[0];
+            string[] arguments = tokens
+                .Skip(1)
+                .ToArray();
+
+            Type commandType = Assembly
+                .GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(m => m.Name == $"{commandName}Command");
+
+            ICommand instance = (ICommand)Activator.CreateInstance(commandType);
+            string executionResult = instance.Execute(arguments);
+
+            return executionResult;
+        }
+    }
+}
---
+++ Reflection And Attributes/Exercise/CommandPattern/Core/Models/Engine.cs
@@ -0,0 +1,31 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+
+namespace CommandPattern.Core.Models
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter command)
+        {
+            this.commandInterpreter = command;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string inputLine = Console.ReadLine();
+                string executionResult = commandInterpreter.Read(inputLine);
+                
+                if (executionResult == null)
+                {
+                    break;
+                }
+
+                Console.WriteLine(executionResult);
+            }
+        }
+    }
+}

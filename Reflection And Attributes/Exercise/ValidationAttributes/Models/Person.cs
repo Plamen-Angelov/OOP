9f516aca909e3d50ebea2ff1b1a262c123341d6a
Plamen Angelov <plamen565@gmail.com>
2021-07-22 14:08:06 +0300

    Add problem
    Add problem to Reflection and Attrinutes exercise

---
+++ Reflection And Attributes/Exercise/ValidationAttributes/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes
+{
+    public class Person
+    {
+        [MyRequired]
+        public string FullName { get; set; }
+
+        [MyRange(12, 90)]
+        public int Age { get; set; }
+
+        public Person(string fullName, int age)
+        {
+            FullName = fullName;
+            Age = age;
+        }
+    }
+}
---
+++ Reflection And Attributes/Exercise/ValidationAttributes/Models/Validator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ValidationAttributes.Attributes;
+using System.Reflection;
+
+namespace ValidationAttributes
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] properties = obj
+                .GetType()
+                .GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                MyValidationAttribute attribute = (MyValidationAttribute)property
+                    .GetCustomAttribute(typeof(MyValidationAttribute), false);
+
+                if(attribute.IsValid(property.GetValue(obj)) == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
